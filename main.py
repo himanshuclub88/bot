a04cfbbbb257e31025b7c1a04bbe25db5fb06782
himanshuclub88 <himanshuclub88@gmail.com>
2025-09-30 01:06:04 +0530

    load chat

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ st.set_page_config(page_title="Two-Person Chat", layout="centered")
 chat_file = "chat_history.txt"
 
 # Callback to add new message
+def laod_chat():
+    if "messages" not in st.session_state:
+        if os.path.exists(chat_file):
+            with open(chat_file, "r", encoding="utf-8") as f:
+                lines = f.readlines()
+            st.session_state.messages = [line.strip() for line in lines][-50:]
+        else:
+            st.session_state.messages = []
+
 def add_message():
     msg = st.session_state.user_input
     if msg:
@@ -16,18 +25,13 @@ def add_message():
             for m in st.session_state.messages:
                 f.write(m + "\n")
         st.session_state.user_input = ""  # Clear input field after submit
+        load_chat()
 
-# Load chat history once
-if "messages" not in st.session_state:
-    if os.path.exists(chat_file):
-        with open(chat_file, "r", encoding="utf-8") as f:
-            lines = f.readlines()
-        st.session_state.messages = [line.strip() for line in lines][-50:]
-    else:
-        st.session_state.messages = []
+
 
 st.title("💬 Two-Person Chat")
 
+load_chat()
 # Display chat messages
 for msg in st.session_state.messages:
     if msg.lower().startswith("bb1"):
