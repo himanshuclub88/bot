5f02e105498991ca5571cda4f1cb5527eff1436a
himanshuclub88 <himanshuclub88@gmail.com>
2025-09-30 00:54:16 +0530

    last 100 message

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 
 st.set_page_config(page_title="Two-Person Chat", layout="centered")
-
+n=100
 chat_file = "chat_history.txt"
 
 # Load previous chat history if file exists
@@ -10,8 +10,8 @@ if "messages" not in st.session_state:
     if os.path.exists(chat_file):
         with open(chat_file, "r", encoding="utf-8") as f:
             lines = f.readlines()
-        # Strip newlines and keep last 50
-        st.session_state.messages = [line.strip() for line in lines][-50:]
+        # Strip newlines and keep last n
+        st.session_state.messages = [line.strip() for line in lines][-n:]
     else:
         st.session_state.messages = []
 
@@ -31,14 +31,13 @@ for msg in st.session_state.messages:
 user_input = st.chat_input("Type a message...")
 
 if user_input:
-    # Append new message, keep last 50
+    # Append new message, keep last n
     st.session_state.messages.append(str(user_input))
-    st.session_state.messages = st.session_state.messages[-50:]
+    st.session_state.messages = st.session_state.messages[-n:]
 
     # Write updated messages back to file (overwrite)
     with open(chat_file, "w", encoding="utf-8") as f:
         for msg in st.session_state.messages:
             f.write(msg + "\n")
 
-    # Rerun to update interface instantly with new message
-    st.rerun()
+
